0858339ed9486f56bb4554fdcefb20a3ea6d11d5
纪洪波 <hongbo.ji@bytedance.com>
2019-06-30 16:27:28 +0800

    修复里数据错误的问题

--- miniprogram/pages/wastList/wastList.js
+++ miniprogram/pages/wastList/wastList.js
@@ -6,7 +6,7 @@ Page({
     onLoad: function (option) {
       var model = getApp().globalData.collections[option.index]
         this.setData({
-          wasts: model
+          wasts: model.wasts
         });
         wx.setNavigationBarTitle({
           title: model.name,
